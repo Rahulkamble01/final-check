95e2a760a7facc56102bf84d8c159f7b0d3d9a4c
Rahul Kamble <rahul.kamble3@cognizant.com>
2019-02-03 15:08:52 +0530

    webservice testing done

--- service/src/test/java/com/cts/articlesearch/restcontroller/SignupControllerMokitoTest.java
+++ service/src/test/java/com/cts/articlesearch/restcontroller/SignupControllerMokitoTest.java
@@ -4,6 +4,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.cts.articlesearch.bean.Language;
 import com.cts.articlesearch.bean.SignupStatus;
 import com.cts.articlesearch.bean.User;
 import com.cts.articlesearch.repository.UserRepository;
@@ -39,22 +42,61 @@ public class SignupControllerMokitoTest {
 	public void signUpNewUser() {
 		LOGGER.info("START : Inside Unit Testing signUpNewUser");
 		User user = new User();
-		user.setEmail("kiran@gmail.com");
+		user.setEmail("rahulkamble@gmail.com");
 		LOGGER.debug("User Object :  {}", user);
 		when(userRepository.findByEmail(user.getEmail())).thenReturn(null);
 
 		SignupStatus status = new SignupStatus(true, "Login to continue....");
 		SignupStatus actualStatus = signupService.save(user);
-				
+		Assert.assertEquals(true, status.equals(actualStatus));
 		LOGGER.debug("status in new signup  Output :  {}", status);
 		LOGGER.debug("Actual  Output :  {}", actualStatus);
 
-		Assert.assertEquals(true, status.equals(actualStatus));
 		verify(userRepository, times(1)).save(Mockito.any(User.class));
 		verify(userRepository, times(1)).findByEmail(user.getEmail());
 
 		LOGGER.info("End : Inside Unit Testing signUpNewUser");
 	}
+	
+	@Test
+	public void alreadyExistEmail() {
+		LOGGER.info("START : Inside Unit Testing alreadyExistEmail");
+		User user = new User();
+		user.setEmail("rahulkamble@gmail.com");
+		LOGGER.debug("User Object :  {}", user);
+		when(userRepository.findByEmail(user.getEmail())).thenReturn(null);
+
+		SignupStatus status = new SignupStatus(false, "Email is alredy exists");
+		SignupStatus actualStatus = signupService.save(user);
+		Assert.assertEquals(false, status.equals(actualStatus));
+		LOGGER.debug("status in new signup  Output :  {}", status);
+		LOGGER.debug("Actual  Output :  {}", actualStatus);
+
+		verify(userRepository, times(1)).save(Mockito.any(User.class));
+		verify(userRepository, times(1)).findByEmail(user.getEmail());
+
+		LOGGER.info("End : Inside Unit Testing alreadyExistEmail");
+	}
+	
+	/*public void languageSelect(){
+		User user = new User();
+		Language language = new Language();
+		language.setName("English");
+		language.setLanguageCode("en");
+		user.setLanguage(language);
+		
+		when(userRepository.findByEmail(user.getLanguage().getLanguageCode())).thenReturn(user);
+		
+		List<Language> lang = (List<Language>) user.getLanguage();
+		List<Language> expectedStatus = signupService.language();
+		
+		LOGGER.debug("lang  Output :  {}", lang);
+		LOGGER.debug("expectedStatus  Output :  {}", expectedStatus);
+		
+		Assert.assertEquals(true, lang.equals(expectedStatus));
+		verify(userRepository, times(0)).save(Mockito.any(User.class));
+		verify(userRepository, times(1)).findByEmail(user.getLanguage().getLanguageCode());
+	}*/
 	
 	
 	
